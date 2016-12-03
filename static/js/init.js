26698f532ffd3679d7adf0770b290474785bc15c
John Diego <jadiego@uw.edu>
2016-12-02 22:18:08 -0800

    Added form to edit database, updated nav to check release database, added release database page

--- static/js/init.js
+++ static/js/init.js
@@ -9,11 +9,42 @@
       transition: 2000
     });
 
+    $('select').material_select();
+
     $(".dropdown-button").dropdown({
       constrain_width: false,
       hover: true, 
       belowOrigin: true,
       alignment: 'left'
     });
+
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+      
+
+    $("#add-ingredient").click(function() {
+      $(".ingredient-row").last().after($(".ingredient-row").first().clone());
+    })
+    
+    $("#add-phase").click(function() {
+      $(".phase-row").last().after($(".phase-row").first().clone());
+    })
+
+    $("#phase-select").change(function() {
+      console.log(this.value);
+      if(this.value == "fermentation") {
+        $(".fermentation-info").removeClass("hide")
+      } else if (this.value == "washdistil") {
+        $(".fermentation-info").addClass("hide")
+      } else if (this.value == "whiskeydistil") {
+        $(".fermentation-info").addClass("hide")
+      } else if (this.value == "mature") {
+        $(".fermentation-info").addClass("hide")
+      } else {
+        $(".fermentation-info").addClass("hide")
+      }
+    })
   }); // end of document ready
 })(jQuery); // end of jQuery name space
