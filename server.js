f710eae3802080e5b123f0a9fdea8584e9e2e630
John Diego <jadiego@uw.edu>
2016-12-04 15:09:00 -0800

    Created /student endpoint to test out SQL Server connection

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 'use strict';
 
 var express = require('express');
+var sql = require('mssql');
+require('dotenv').config();
 
 var app = express()
-
-var server = 8080
-
 app.use(express.static(__dirname + '/static'));
+
+app.get('/student', function (req, res) {
+    var config = {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        server: process.env.DB_HOST,
+        database: process.env.DB_NAME
+    }
+
+    sql.connect(config, function (err) {
+        if (err) 
+            console.log(err);
+        
+         var request = new sql.Request();
+
+         request.query('select * from Student', function (err, recordset) {
+            if (err) console.log(err)
+            res.send(recordset);
+            console.log(recordset)
+        });
+    })
+})
+
+var server = 8080;
 
 app.listen(8080, function() {
     console.log('Server is listening on port: ' + server)
 });
-
