1925aa816e45fe02f2a7d69721b184801d4df5d7
John D <jadiego@uw.edu>
2016-12-06 18:08:47 -0800

    Added Angular

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+angular.module('CopperWorksApp', [])
+    .controller('DBReleaseController', ['$scope', '$http', function($scope, $http) {
+        'use strict';
+        $http.get('/api/releases').success(function(data) {
+            $scope.brewid = data.BrewID;
+            $scope.brewnum = data.BrewNum;
+            $scope.name = data.BrewName;
+            $scope.date = data.BrewDate;
+        })
+    }])
